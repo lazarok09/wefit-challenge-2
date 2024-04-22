bbbb5b90fb4d91a4fc5f34fa74b898e35a8cc889
lazarok09 <lazarok09@gmail.com>
2024-04-22 19:34:24 -0300

    refactor: improve main code and add success to the button

--- js-assets/wefit-test/js/main.js
+++ js-assets/wefit-test/js/main.js
@@ -2,7 +2,6 @@
 const menu = document.querySelector(".btn-group-vertical");
 menu.classList.add("menu");
 
-
 // HEADER
 const header = document.querySelector(".jumbotron");
 
@@ -19,12 +18,7 @@ if (header) {
 const cards = document.querySelectorAll(".card");
 if (cards?.length) {
   // Array to store the HTML elements of the cards in the original order
-  const reorganizedElements = [];
-
-  // Store the HTML elements of the cards in the original order
-  for (const card of cards) {
-    reorganizedElements.push(card);
-  }
+  const reorganizedElements = Array.from(cards);
 
   // Remove and keep the last element
   const lastElement = reorganizedElements.pop();
@@ -35,16 +29,12 @@ if (cards?.length) {
   // Swap the last and second-to-last elements to restore the original order
   const newLast = reorganizedElements.pop();
   const penultimate = reorganizedElements.pop();
-  reorganizedElements.push(newLast);
-  reorganizedElements.push(penultimate);
+  reorganizedElements.push(newLast, penultimate);
 
   // Array to store the parent elements of the cards
-  const cardParents = [];
 
   // Store the parent elements of the cards
-  cards.forEach((card) => {
-    cardParents.push(card.parentElement);
-  });
+  const cardParents = Array.from(cards, (card) => card.parentElement);
 
   // Remove all cards from the DOM
   cards.forEach((card) => card.remove());
@@ -52,6 +42,12 @@ if (cards?.length) {
   // Insert the reorganized cards back into the DOM in the correct order
   reorganizedElements.forEach((card, index) => {
     cardParents[index].appendChild(card);
+
+    // Change the button color
+    if (index === 1) {
+      const cardButton = card.querySelector(".btn-primary");
+      cardButton.classList.add("success-button");
+    }
   });
 }
 // END CARDS
